e4b2c1251d5e8b455dbc3b59ac94d9611fa8ad97
Radosław Korzeniewski <radekk@inteos.pl>
2020-02-06 13:24:09 +0100

    Create unit tests for pool.

--- src/test_thread_pool.cpp
+++ src/test_thread_pool.cpp
@@ -16,103 +16,196 @@
 
 #include <iostream>
 #include <random>
+#include <utility>
 #include "ThreadPool.h"
 #include "catch.hpp"
 
 
 std::random_device rd;
 std::mt19937 mt(rd());
-std::uniform_int_distribution<int> dist(-1000, 1000);
+std::uniform_int_distribution<int> dist(0, 10);
 auto rnd = std::bind(dist, mt);
 std::atomic<int> counter = {0};
 
+// waits a random time which simulate random completition time
 void simulate_hard_computation()
 {
-   std::this_thread::sleep_for(std::chrono::milliseconds(200 + rnd()));
+   std::this_thread::sleep_for(std::chrono::milliseconds(1 + rnd()));
 }
 
-void test_thread1()
+// testing thread, no return, simulate computation time
+void test_thread_void()
 {
+   counter++;
    simulate_hard_computation();
+}
+
+// testing thread, no return no computation time
+void test_thread_none()
+{
    counter++;
 }
 
-// Simple function that adds multiplies two numbers and prints the result
-void multiply(const int a, const int b) {
+// testing thread, single parameter and return value, simulate computation time
+int test_thread_p1r(const int a)
+{
    simulate_hard_computation();
-   const int res = a * b;
-   std::cout << a << " * " << b << " = " << res << std::endl;
+   return a;
 }
 
-// Same as before but now we have an output parameter
-void multiply_output(int & out, const int a, const int b) {
+// testing thread, double parameters and return value, simulate computation time
+uint64_t test_thread_p2r(const int a, const int b)
+{
+   const auto res = a * b;
    simulate_hard_computation();
-   out = a * b;
-   std::cout << a << " * " << b << " = " << out << std::endl;
-}
-
-// Same as before but now we have an output parameter
-int multiply_return(const int a, const int b) {
-   simulate_hard_computation();
-   const int res = a * b;
-   std::cout << a << " * " << b << " = " << res << std::endl;
    return res;
 }
 
-TEST_CASE ("Execute simple tasks")
+// testing thread, triple parameters (including reference) and return value, simulate computation time
+int test_thread_p3rr(uint64_t & out, const int a, const int b)
 {
-   ThreadPool pool(4);
-   pool.init();
+   out = a * b;
+   simulate_hard_computation();
+   return b;
+}
 
-   for (auto i = 0; i < 8; i++){
-      pool.submit(test_thread1);
-   }
-   pool.shutdown();
+void wait_for_pool_to_complete(ThreadPool &pool)
+{
+   do {
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   } while (pool.queue_size() > 0 || pool.num_running() > 0);
+}
 
-   CHECK (counter == 4);
+const auto test_vector1 = {1, 2, 3, 4, 6, 8, 16, 128, 256, 1024, 65536};
+const auto test_vector2 = {1, 2, 4, 8, 16, 23, 32, 41, 89, 64, 127, 283, 353};
+const std::vector<std::pair<int, int>> test_vector3 = {
+   {45420, 30014},      // random set of numbers
+   {56140, 6248},
+   {13509, 36300},
+   {2920,  20522},
+   {27365, 23315},
+   {40514, 37448},
+   {34605, 54379},
+   {10200, 12722},
 };
 
-TEST_CASE ("Execute simple tasks with abort")
+TEST_CASE ("Default ctor", "defctor")
 {
-   ThreadPool pool(4);
-   pool.init();
-
-   for (auto i = 0; i < 8; i++){
-      pool.submit(test_thread1);
+   SECTION ("default"){
+      ThreadPool pool;
+      CHECK ( pool.size() == std::thread::hardware_concurrency());
+      CHECK_FALSE ( pool.num_available() > 0 );
    }
-   pool.shutdown(true);
 
-   CHECK (counter != 4);
+   SECTION ("zero")
+   {
+      ThreadPool pool(0);
+      CHECK ( pool.size() == std::thread::hardware_concurrency() );
+      CHECK_FALSE ( pool.num_available() > 0 );
+   }
 };
 
-void example() {
-   // Create pool with 3 threads
-   ThreadPool pool(3);
+TEST_CASE ("Explicit ctor", "exctor")
+{
+   ThreadPool * pool;
 
-   // Initialize pool
+   for (auto v : test_vector1){
+      pool = new ThreadPool(v);
+      REQUIRE ( pool != nullptr );
+      CHECK ( pool->size() == v );
+      CHECK_FALSE ( pool->num_available() > 0 );
+      delete pool;
+   }
+};
+
+TEST_CASE ("Job execution", "jobexec")
+{
+   ThreadPool pool;
    pool.init();
 
-   // Submit (partial) multiplication table
-   for (auto i = 1; i < 3; ++i) {
-      for (auto j = 1; j < 10; ++j) {
-         pool.submit(multiply, i, j);
+   for (auto v : test_vector2){
+      counter = 0;
+      for (auto n = 0; n < v; n++){
+         pool.submit(test_thread_void);
       }
+      wait_for_pool_to_complete(pool);
+      CHECK ( counter == v );
+      CHECK ( pool.num_available() > 0 );
+      CHECK_FALSE ( pool.num_running() > 0 );
    }
+};
 
-   // Submit function with output parameter passed by ref
-   int output_ref;
-   auto future1 = pool.submit(multiply_output, std::ref(output_ref), 5, 6);
+TEST_CASE ("Job execution single param", "jobexecsingle")
+{
+   ThreadPool pool;
+   pool.init();
 
-   // Wait for multiplication output to finish
-   future1.get();
-   std::cout << "Last operation result is equals to " << output_ref << std::endl;
+   for (auto v : test_vector2){
+      auto future = pool.submit(test_thread_p1r, v);
+      // wait for result
+      auto res = future.get();
+      CHECK ( res == v );
+      CHECK ( pool.num_available() > 0 );
+      CHECK_FALSE ( pool.num_running() > 0 );
+   }
+};
 
-   // Submit function with return parameter
-   auto future2 = pool.submit(multiply_return, 5, 3);
+TEST_CASE ("Job execution double param", "jobexecdouble")
+{
+   ThreadPool pool;
+   pool.init();
 
-   // Wait for multiplication output to finish
-   int res = future2.get();
-   std::cout << "Last operation result is equals to " << res << std::endl;
+   for (auto p : test_vector3){
+      auto a = std::get<0>(p);
+      auto b = std::get<1>(p);
+      auto future = pool.submit(test_thread_p2r, a, b);
+      // wait for result
+      auto res = future.get();
+      CHECK ( res == (a * b) );
+      CHECK ( pool.num_available() > 0 );
+      CHECK_FALSE ( pool.num_running() > 0 );
+   }
+};
 
+TEST_CASE ("Job execution triple param", "jobexectriple")
+{
+   ThreadPool pool;
+   pool.init();
+   uint64_t out;
+
+   for (auto p : test_vector3){
+      auto a = std::get<0>(p);
+      auto b = std::get<1>(p);
+      out = 0;
+      auto future = pool.submit(test_thread_p3rr, std::ref(out), a, b);
+      // wait for result
+      auto res = future.get();
+      CHECK ( res == b );
+      CHECK ( out == (a * b) );
+      CHECK ( pool.num_available() > 0 );
+      CHECK_FALSE ( pool.num_running() > 0 );
+   }
+};
+
+TEST_CASE ("Init after shutdown", "shutinit")
+{
+   ThreadPool pool;
+
+   // submit single job to execute
+   pool.submit(test_thread_void);
+   CHECK ( pool.queue_size() == 1 );
+
+   // test shutdown without init
    pool.shutdown();
+   CHECK_FALSE ( pool.num_available() > 0 );
+   CHECK ( pool.queue_size() == 1 );
+
+   // now do init and wait for job to complete
+   pool.init();
+   wait_for_pool_to_complete(pool);
+   CHECK ( pool.num_available() > 0 );
+
+   // test final shutdown
+   pool.shutdown();
+   CHECK_FALSE ( pool.num_available() > 0 );
 }
